9018368ffe20c2a529a33797e22e068ddfbe579e
koushenhai <2413176044@qq.com>
2025-03-05 17:06:35 +0800

    111

--- reactor-test/src/main/java/test/Test6.java
+++ reactor-test/src/main/java/test/Test6.java
@@ -2,14 +2,16 @@ package test;
 
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.HostAccess;
+import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 
 import java.util.Map;
 
 public class Test6 {
 
-    private static final Context GLOBAL_CONTEXT = Context.newBuilder()
+    private static final Context GLOBAL_CONTEXT = Context.newBuilder("js")
             .allowAllAccess(true)
+            .option("js.esm-eval-returns-exports", "true")  // 支持 ES 模块
             .option("engine.WarnInterpreterOnly", "false") // 禁用解释模式警告
             .build();
 
@@ -31,15 +33,11 @@ public class Test6 {
                 })
                 """;
 
-        // 3. 执行 JavaScript 代码获取函数对象
-        Value jsFunction = GLOBAL_CONTEXT.eval("js", jsCode);
+        Source source = Source.create("js", jsCode);
+        // 执行函数并返回结果
+        Object o = GLOBAL_CONTEXT.eval(source).execute(map).as(Object.class);
 
-        // 4. 调用 JS 函数并传入 Map
-        String result = jsFunction.execute(map).asString();
-
-        GLOBAL_CONTEXT.close();
-
-        System.out.println("结果: " + result);
+        System.out.println("结果: " + o);
         System.out.println("耗时: " + (System.currentTimeMillis() - startTime) + " 毫秒");
     }
 
